bc319fac51d1e92d68968b3c0dc98663c6ab5177
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-07-23 19:56:44 +0530

    Nike Landing Page added

---
+++ 01-nike-landing-page/src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Navigation from "./components/Navigation";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
